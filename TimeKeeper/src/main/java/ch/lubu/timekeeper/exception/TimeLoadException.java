fbc8f978664923b1388f6926bd0d4d3f43b83710
thisisvv <58626061+thisisvv@users.noreply.github.com>
2022-09-22 21:41:08 +0200

    deleted and archived old version

--- TimeKeeper/src/main/java/ch/lubu/timekeeper/exception/TimeLoadException.java
+++
@@ -1,7 +0,0 @@
-package ch.lubu.timekeeper.exception;
-
-public class TimeLoadException extends RuntimeException {
-    public TimeLoadException() {
-        super("Times could not be loaded.");
-    }
-}
--- TimeKeeper/src/main/java/ch/lubu/timekeeper/model/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.lubu.timekeeper.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
-
-}
